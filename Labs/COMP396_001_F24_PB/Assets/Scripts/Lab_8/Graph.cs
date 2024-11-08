d925bac792ee189720ff52da85029d81b64a704e
Plaban Biswas <plabanbiswas96@outlook.com>
2024-11-08 14:32:55 -0500

    - ADDED: LAB - 8

--- Labs/COMP396_001_F24_PB/Assets/Scripts/Lab_8/Graph.cs
+++ Labs/COMP396_001_F24_PB/Assets/Scripts/Lab_8/Graph.cs
@@ -1,10 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.Rendering;
-
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 namespace PathFinding
 {
@@ -17,12 +16,45 @@ namespace PathFinding
             vertices[vertex] = edges;
         }
 
-        public List<char> ShortestPathViaDijkstra(char start, char finish)
+        public void TestDFS()
+        {
+            var graph = new Dictionary<string, List<string>>
+            {
+                { "A", new List<string> { "B", "C" } },
+                { "B", new List<string> { "A", "D", "E" } },
+                { "C", new List<string> { "A", "F" } },
+                { "D", new List<string> { "B" } },
+                { "E", new List<string> { "B", "F" } },
+                { "F", new List<string> { "C", "E" } }
+            };
+
+            var discovered = new HashSet<string>();
+            DepthFirstSearch(graph, "A", discovered);
+        }
+
+        void DepthFirstSearch(Dictionary<string, List<string>> graph, string start, HashSet<string> discovered)
+        {
+            // Label the start vertex as discovered
+            discovered.Add(start);
+            Debug.Log(start);
+
+            // Explore each adjacent vertex
+            foreach (var neighbor in graph[start])
+            {
+                if (!discovered.Contains(neighbor))
+                {
+                    DepthFirstSearch(graph, neighbor, discovered);
+                }
+            }
+        }
+
+
+        public OptimalPath ShortestPathViaDijkstra(char start, char finish)
         {
             //initialize
             List<char> path = new List<char>();
             var distances = new Dictionary<char, int>();
-            var previous = new Dictionary<char, char>();
+            var previous = new Dictionary<char, char>(); // Parents
             var Pending = new List<char>();
 
             //step 0
@@ -32,18 +64,74 @@ namespace PathFinding
                 previous[v.Key] = '\0';
                 Pending.Add(v.Key);
             }
+
             distances[start] = 0;
+
             //main loop
             while (Pending.Count > 0)
             {
+                // Priority Queue Sort
                 Pending.Sort((x, y) => distances[x].CompareTo(distances[y]));
-                var u = Pending[0];
-                // TODO
-                // ...
-                // ...
+                var currentVisited = Pending[0];
+                Pending.RemoveAt(0);
+
+                foreach (var adjacentVertex in vertices[currentVisited])
+                {
+                    var weight = adjacentVertex.Value;
+                    var node = adjacentVertex.Key;
+
+                    if (distances[node] > distances[currentVisited] + weight)
+                    {
+                        distances[node] = weight + distances[currentVisited];
+                        previous[node] = currentVisited;
+                    }
+                }
             }
 
+            return ConstructOptimalPath(distances, previous, start, finish);
+        }
+
+        OptimalPath ConstructOptimalPath(Dictionary<char, int> distances, Dictionary<char, char> previous, char start, char finish)
+        {
+            var optimalDistance = distances[finish];
+
+            var pathList = new List<char>();
+
+            var currentNode = finish;
+
+            while (currentNode != '\0')
+            {
+                pathList.Add(currentNode);
+                var tempNode = previous[currentNode];
+                currentNode = tempNode;
+            }
+
+            pathList.Reverse();
+
+            return new OptimalPath(pathList, optimalDistance);
+        }
+
+    }
+
+    public class OptimalPath
+    {
+        private readonly List<char> path;
+        private readonly int length;
+
+        public OptimalPath(List<char> path, int length)
+        {
+            this.path = path;
+            this.length = length;
+        }
+
+        public List<char> GetPath()
+        {
             return path;
+        }
+
+        public int GetLength()
+        {
+            return length;
         }
     }
 }
--- Labs/COMP396_001_F24_PB/Assets/Scripts/Lab_8/TestDijkstraAlgorithm.cs
+++ Labs/COMP396_001_F24_PB/Assets/Scripts/Lab_8/TestDijkstraAlgorithm.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using Unity.VisualScripting;
+
 using UnityEngine;
 
 namespace PathFinding
@@ -12,7 +14,7 @@ namespace PathFinding
         {
             Graph g = new Graph();
 
-            g.AddVertex('A', new Dictionary<char, int>() 
+            g.AddVertex('A', new Dictionary<char, int>()
             {
                 {'B', 10 },
                 {'C', 12 },
@@ -20,37 +22,65 @@ namespace PathFinding
                 {'E', 2 }
             });
 
-            g.AddVertex('B', new Dictionary<char, int>() 
+            g.AddVertex('B', new Dictionary<char, int>()
             {
-                {'C', 2 }, 
-                {'D', 4 }, 
-                {'E', 5 }, 
+                {'C', 2 },
+                {'D', 4 },
+                {'E', 5 },
                 {'F', 5 }
             });
 
-            g.AddVertex('C', new Dictionary<char, int>() 
+            g.AddVertex('C', new Dictionary<char, int>()
             {
-                {'B', 6 }, 
+                {'B', 6 },
                 {'F', 2 }
             });
 
-            g.AddVertex('D', new Dictionary<char, int>() 
+            g.AddVertex('D', new Dictionary<char, int>()
             {
-                {'B', 3 }, 
+                {'B', 3 },
                 {'E', 3 }
             });
 
-            g.AddVertex('E', new Dictionary<char, int>() 
+            g.AddVertex('E', new Dictionary<char, int>()
             {
-                {'B', 3 }, 
-                {'D', 3 }, 
+                {'B', 3 },
+                {'D', 3 },
                 {'F', 9 }
             });
 
             g.AddVertex('F', new Dictionary<char, int>());
 
             print("g:" + g);
-            //List<char> shortest_path = g.ShortestPathViaDijkstra('A', 'F');
+
+            char start = 'A';
+            char end = 'F';
+
+            var shortest_path = g.ShortestPathViaDijkstra(start, end);         
+
+            Debug.Log("The shortest path: " + GetVisualizedPath(shortest_path.GetPath(), end));
+            Debug.Log("Length of the shortest path: " + shortest_path.GetLength());
+
+            Debug.Log("----- DFS TEST START -----");
+            g.TestDFS();
+            Debug.Log("----- DFS TEST END -----");
+        }
+
+        private string GetVisualizedPath(List<char> path, char end)
+        {
+            var pathVisualizer = "";
+
+            foreach (var node in path)
+            {
+                pathVisualizer += node.ToString();
+
+                if (node != end)
+                {
+                    pathVisualizer += " -> ";
+                }
+            }
+
+            return pathVisualizer;
         }
     }
 }
